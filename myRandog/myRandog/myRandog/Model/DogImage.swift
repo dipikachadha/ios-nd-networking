271e2c4f686a69fe19bdd17f96ecf854bb4909d4
Dipika Chadha <dipika.chadha@icloud.com>
2019-07-23 20:56:15 -0400

    Add missing file

---
+++ myRandog/myRandog/myRandog/Model/DogImage.swift
@@ -0,0 +1,14 @@
+//
+//  DogImage.swift
+//  myRandog
+//
+//  Created by DCJ Indie Apps on 7/23/19.
+//  Copyright © 2019 Dipika Chadha. All rights reserved.
+//
+
+import Foundation
+
+struct DogImage: Codable {
+    let status: String
+    let message: String
+}
